5a97b9785e0a1455f39a50ba807c078cfd879517
Brady Hawkins <brady93hawkins@gmail.com>
2024-04-01 22:14:31 +0200

    added client & htmx

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import fs from 'fs';
+import express from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get( '/', (req, res) => {
-				res.sendFile(__dirname);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+	const videoFiles = getVideoFiles();
+	res.send(generateVideoPage(videoFiles));
 });
 
-//app.get
-app.get('/video', (req, res) => {
-const range = req.headers.range;
+app.get("/video/:id", (req, res) => {
+	const videoId = req.params.id;
+	const videoFilePath = path.join(__dirname, `/video/${videoId}`);
 
-const videoPath = ('/video/Iron.Man.mp4');
-
-const videoSize = fs.statSync('/video/Iron.Man.mp4').size;
-const CHUNK_SIZE = 10 ** 6;
-const start = Number(range?.replace(\/D/,""));
-const end = Math.min(start + CHUNK_SIZE, videoSize -1);
-const contentLength = end - start +1;
-const headers = {  "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-
-		res.writeHead(206, headers);
-		const videoStream = fs.createReadStream(videoPath, {start, end});
-		videoStream.pipe(res);
+	res.sendFile(path.resolve(videoFilePath));
 });
 
+function getVideoFiles(): string[] {
+	const videoDir = (path.join(__dirname, "/video/"));
+	return fs.readdirSync(videoDir).filter((file) => file.endsWith(".mp4"));
+}
 
-app.listen(PORT, () => {	
-				console.log(`[server]: Server is running at localhost:${PORT}`);
+function generateVideoPage(videoFiles: string[]): string {
+	let html = "";
+	videoFiles.forEach((videoFile) => {
+		html += `<li><a href="/video/${videoFile.toString()}">${videoFile}</a></li>`;
+	});
+	html += "</ul>";
+	return html;
+}
 
+app.listen(PORT, () => {
+	console.log(`[server]: Server is running at localhost:${PORT}`);
 });
